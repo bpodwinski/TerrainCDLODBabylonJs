7800b202911ddab0ae7a041c877b58d3a9b97bd3
Benoit Podwinski <me@benoitpodwinski.com>
2025-02-24 18:02:36 +0100

    Fix url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
   return {
-    base: "/terraincdlodbabylonjs/",
+    base: "/TerrainCDLODBabylonJs/",
     resolve: {
       alias: {
         babylonjs:
